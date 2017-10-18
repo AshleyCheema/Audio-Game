8026ece17f4380b7f7661960841d6e76ae830682
IHudson <cuffy94@gmail.com>
2017-10-18 15:41:41 +0100

    Updated
    AI moves around number changes audio listener

---
+++ Assets/Scripts/AI/AIWaypointMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWaypointMovement : MonoBehaviour
+{
+	public GameObject aiCharacter;
+
+	public float minDistance;
+	public float moveSpeed;
+	public GameObject goWayPointParent;
+	Transform[] waypoints;
+	int waypointIndex;
+
+	// Use this for initialization
+	void Start ()
+	{
+		waypointIndex = 0;
+
+		waypoints = new Transform[goWayPointParent.transform.childCount];
+		for ( int i = 0; i < goWayPointParent.transform.childCount; ++i )
+		{
+			waypoints[i] = goWayPointParent.transform.GetChild(i).transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Vector3.Distance(aiCharacter.transform.position, waypoints[waypointIndex].position) < minDistance)
+		{
+			if ( waypointIndex < waypoints.Length - 1 )
+			{
+				waypointIndex++;
+			}
+			else
+			{
+				waypointIndex = 0;
+			}
+		}
+		else
+		{
+			aiCharacter.transform.position = Vector3.MoveTowards(aiCharacter.transform.position, waypoints[waypointIndex].position, moveSpeed * Time.deltaTime);
+			aiCharacter.transform.LookAt(waypoints[waypointIndex].position);
+		}
+	}
+}
---
+++ Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+	public GameObject goAudioLis;
+	public GameObject goAudioListners;
+	Transform[] tranAudioListeners;
+
+	// Use this for initialization
+	void Start ()
+	{
+		tranAudioListeners = new Transform[goAudioListners.transform.childCount];
+		for ( int i = 0; i < goAudioListners.transform.childCount; ++i )
+		{
+			tranAudioListeners[i] = goAudioListners.transform.GetChild(i).transform;
+			tranAudioListeners[i].gameObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SetListenerPosition(0);
+		}
+		if ( Input.GetKeyDown(KeyCode.Alpha2) )
+		{
+			SetListenerPosition(1);
+		}
+		if ( Input.GetKeyDown(KeyCode.Alpha3) )
+		{
+			SetListenerPosition(2);
+		}
+	}
+
+	void SetListenerPosition(int a_audioIndex)
+	{
+		goAudioLis.transform.SetPositionAndRotation(tranAudioListeners[a_audioIndex].position, Quaternion.identity);
+		ActivateAudioSource(a_audioIndex);
+	}
+
+	void ActivateAudioSource( int a_audioIndex )
+	{
+		for ( int i = 0; i < tranAudioListeners.Length; ++i )
+		{
+			if ( i == a_audioIndex )
+			{
+				tranAudioListeners[a_audioIndex].gameObject.SetActive(true);
+			}
+			else
+			{
+				if(tranAudioListeners[i].gameObject.activeInHierarchy)
+				{
+					tranAudioListeners[i].gameObject.SetActive(false);
+				}
+			}
+		}
+	}
+}
